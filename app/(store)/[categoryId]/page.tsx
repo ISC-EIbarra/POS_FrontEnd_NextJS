393b9c69fcd5bfae084c1f0b145eb8f9dc60ff47
Edson <EdsonP4100@gmail.com>
2026-05-11 14:46:12 -0700

    first commit

---
+++ app/(store)/[categoryId]/page.tsx
@@ -0,0 +1,13 @@
+type Params = Promise<{ categoryId: string }>;
+
+async function getProducts(categoryId: string) {
+  const url = `${process.env.API_URL}/categories/${categoryId}?products=true`;
+  const req = await fetch(url);
+  const json = await req.json();
+  console.log(json);
+}
+export default async function StorePage({ params }: { params: Params }) {
+  const { categoryId } = await params;
+  await getProducts(categoryId);
+  return <div>Store page</div>;
+}
