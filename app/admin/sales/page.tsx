62df209f4b574e88012a20fc3f41c0cfc3469aa1
Edson <EdsonP4100@gmail.com>
2026-07-30 17:58:49 -0700

    Added calendar

---
+++ app/admin/sales/page.tsx
@@ -0,0 +1,34 @@
+import TransactionFilter from '@/components/transactions/TransactionFilter';
+import Heading from '@/components/ui/Heading';
+import {
+  QueryClient,
+  dehydrate,
+  HydrationBoundary,
+} from '@tanstack/react-query';
+import { format } from 'date-fns';
+
+export default function SalesPage() {
+  const queryClient = new QueryClient();
+
+  const today = new Date();
+  const formattedDate = format(today, 'yyyy-MM-dd');
+
+  // await queryClient.prefetchQuery({
+  //   queryKey: ['sales', formattedDate],
+  //   queryFn:
+  // });
+
+  return (
+    <>
+      <Heading>Ventas</Heading>
+      <p className="text-lg">
+        En esta sección aparecerán las ventas, utiliza el calendario para
+        filtrar por fecha.
+      </p>
+
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <TransactionFilter />
+      </HydrationBoundary>
+    </>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Outfit } from 'next/font/google';
 import './globals.css';
+import Providers from './providers';
 
 const outfit = Outfit({ subsets: ['latin'] });
 
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${outfit.className} bg-gray-200`}>
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
