8236e6b6fbbc8ae98353a958255364d38877d218
Edson <EdsonP4100@gmail.com>
2026-08-20 13:44:23 -0700

    added products page

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,3 +1,25 @@
-export default function ProductsPage() {
-  return <div>Products Page</div>;
+import ProductsTable from '@/components/products/ProductsTable';
+import Heading from '@/components/ui/Heading';
+import { ProductResponseSchema } from '@/src/schemas';
+
+async function getProducts() {
+  const url = `${process.env.API_URL}/products`;
+  const req = await fetch(url);
+  const json = await req.json();
+  const data = ProductResponseSchema.parse(json);
+
+  return {
+    products: data.products,
+    total: data.total,
+  };
+}
+
+export default async function ProductsPage() {
+  const { products, total } = await getProducts();
+  return (
+    <>
+      <Heading>Administrar Productos</Heading>
+      <ProductsTable products={products} />
+    </>
+  );
 }
