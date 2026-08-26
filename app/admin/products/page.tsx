937e448e9f36663455412e22a94ea5d138253305
Edson <EdsonP4100@gmail.com>
2026-08-25 17:49:31 -0700

    Added pagination, product list

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,9 +1,13 @@
 import ProductsTable from '@/components/products/ProductsTable';
 import Heading from '@/components/ui/Heading';
+import Pagination from '@/components/ui/Pagination';
 import { ProductResponseSchema } from '@/src/schemas';
+import { isValidPage } from '@/src/utils';
+import Link from 'next/link';
+import { redirect } from 'next/navigation';
 
-async function getProducts() {
-  const url = `${process.env.API_URL}/products`;
+async function getProducts(take: number, skip: number) {
+  const url = `${process.env.API_URL}/products?take=${take}&skip=${skip}`;
   const req = await fetch(url);
   const json = await req.json();
   const data = ProductResponseSchema.parse(json);
@@ -14,12 +18,37 @@ async function getProducts() {
   };
 }
 
-export default async function ProductsPage() {
-  const { products, total } = await getProducts();
+type SearchParams = Promise<{ page: string }>;
+
+export default async function ProductsPage({
+  searchParams,
+}: {
+  searchParams: SearchParams;
+}) {
+  const { page } = await searchParams;
+  if (!isValidPage(+page)) redirect('/admin/products?page=1');
+  const productsPerPage = 10;
+  const skip = (+page - 1) * productsPerPage;
+
+  const { products, total } = await getProducts(productsPerPage, skip);
+
+  const totalPages = Math.ceil(total / productsPerPage);
+  if (+page > totalPages) redirect('/admin/products?page=1');
   return (
     <>
+      <Link
+        href="/admin/products/new"
+        className="rounded bg-green-400 font-bold py-2 px-10 hover:bg-green-500 transition-colors cursor-pointer"
+      >
+        Nuevo Producto
+      </Link>
       <Heading>Administrar Productos</Heading>
       <ProductsTable products={products} />
+      <Pagination
+        page={+page}
+        totalPages={totalPages}
+        baseUrl="/admin/products"
+      />
     </>
   );
 }
