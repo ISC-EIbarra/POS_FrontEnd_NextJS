e6cd4659ba76807872172baedc50687badc8491f
Edson <EdsonP4100@gmail.com>
2026-09-08 14:44:26 -0700

    Add edit and delete products

--- app/admin/products/[id]/edit/page.tsx
+++ app/admin/products/[id]/edit/page.tsx
@@ -1,9 +1,29 @@
 import EditProductForm from '@/components/products/EditProductForm';
 import ProductForm from '@/components/products/ProductForm';
 import Heading from '@/components/ui/Heading';
+import { ProductSchema } from '@/src/schemas';
 import Link from 'next/link';
+import { notFound } from 'next/navigation';
 
-export default function EditProductPage() {
+async function getProduct(id: string) {
+  const url = `${process.env.API_URL}/products/${id}`;
+  const req = await fetch(url);
+  const json = await req.json();
+
+  if (!req.ok) {
+    notFound();
+  }
+
+  const product = ProductSchema.parse(json);
+  return product;
+}
+
+type Params = Promise<{ id: string }>;
+
+export default async function EditProductPage({ params }: { params: Params }) {
+  const { id } = await params;
+  const product = await getProduct(id);
+
   return (
     <>
       <Link
@@ -13,10 +33,10 @@ export default function EditProductPage() {
         Volver
       </Link>
 
-      <Heading>Editar Producto: {}</Heading>
+      <Heading>Editar Producto: {product.name}</Heading>
 
       <EditProductForm>
-        <ProductForm />
+        <ProductForm product={product} />
       </EditProductForm>
     </>
   );
