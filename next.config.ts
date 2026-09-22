b6d5c6fba6d6a78a14f8126247eda474e6eca782
Edson <EdsonP4100@gmail.com>
2026-09-21 18:17:57 -0700

    Add image upload and edit

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,14 @@ const nextConfig: NextConfig = {
         protocol: 'http',
         hostname: process.env.DOMAIN!,
       },
+      {
+        protocol: 'https',
+        hostname: process.env.DOMAIN!,
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
     ],
   },
 };
